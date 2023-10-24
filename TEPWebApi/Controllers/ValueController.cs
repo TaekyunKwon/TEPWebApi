73b9eb60599084be9a08c9aa9f090ffed147d708
Ted Kwon <kwon.taekyun@tusd.org>
2023-10-24 14:20:10 -0700

    commit/

---
+++ TEPWebApi/Controllers/ValueController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TEPWebApi.Controllers
+{
+    public class ValueController : ApiController
+    {
+        public string Get() {
+            return "API test in local sesrver.";
+        }
+
+        public void Post([FromBody] string value)
+        {
+
+        }
+    }
+}
---
+++ TEPWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TEPWebApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Value1", "Value2" };
+        }
+
+        [HttpGet]
+        public IEnumerable<string> TEP()
+        {
+            return new string[] { "Value1", "Value2" };
+        }
+
+        //Get api/values?id=
+        public string Get(int id)
+        {
+            return "Value-ID";
+        }
+
+        //Post api/values
+        public void Post([FromBody]string value)
+        {
+
+        }
+
+        //Put api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+
+        }
+
+        //Delete api/values/5
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
